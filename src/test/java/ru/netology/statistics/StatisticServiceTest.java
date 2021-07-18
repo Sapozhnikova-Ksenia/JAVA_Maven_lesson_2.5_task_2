586fa0982bc7ff6cdc1bcd9fd242907bcf930613
Sapozhnikova-Ksenia <mobillka92@gmail.com>
2021-07-18 21:55:48 +0300

    test crush

--- src/test/java/ru/netology/statistics/StatisticServiceTest.java
+++ src/test/java/ru/netology/statistics/StatisticServiceTest.java
@@ -9,7 +9,7 @@ class StatisticsServiceTest {
     void findMax() {
         StatisticServise service = new StatisticServise();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 250};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 2500000000000000};
         long expected = 250;
 
         long actual = service.findMax(incomesInBillions);
